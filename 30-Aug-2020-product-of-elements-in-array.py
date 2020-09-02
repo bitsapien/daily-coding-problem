0eadf2e6cde913d1668ef76172ab1ab33ae7a8bf
bitsapien <bitsapien@gmail.com>
2020-09-02 09:36:46 +0530

    product-of-elements-in-array

---
+++ 30-Aug-2020-product-of-elements-in-array.py
@@ -0,0 +1,16 @@
+# Problem
+#  Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+# For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+#
+# TODO Follow-up: what if you can't use division?
+
+import functools
+
+arr = [1, 2, 3, 4, 5]
+
+def product_of_elements_in_array(arr):
+    full_prod = functools.reduce(lambda x, y: x*y, arr)
+    return map(lambda x: full_prod/x, arr)
+
+
+print(product_of_elements_in_array(arr))
